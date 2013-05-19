55685a30ff69774bfbebc86d87a0893f24ac5030
Tim Kinnel <tim@vivoom.co>
2013-05-19 10:15:39 -0400

    Simplified data object usage. Now returns an Image object which contains convenient variables.

--- jquery.icon_crop.js
+++ jquery.icon_crop.js
@@ -1,26 +1,35 @@
 (function($) {
-  var Box = function(w, h) {
+  var Image = function(w, h, left, top) {
     this.w = w;
     this.h = h;
+    this.o_w = w;
+    this.o_h = h;
+    this.left = left || 0;
+    this.top = top = top || 0;
+    this.right = this.left + w;
+    this.bottom = this.top + h
     this.original_w = w;
     this.original_h = h;
-    this.zoom = function(zoom) {
+    this.resize = function(zoom, crop) {
       this.w = this.original_w / zoom.now;
       this.h = this.original_h / zoom.now;
+      this.zoom = zoom.now
+      this.left = (this.left - crop.w/2) * (zoom.previous/zoom.now) + crop.w/2;
+      this.top = (this.top - crop.h/2) * (zoom.previous/zoom.now) + crop.h/2;
+      this.right = this.left + this.w
+      this.bottom = this.top + this.h
     }
-  }
-  var Offset = function(x, y) {
-    this.x = x;
-    this.y = y;
-    this.original_x = x;
-    this.original_y = y;
-    this.translate = function(position) {
-      this.x = position.left;
-      this.y = position.top;
+    this.translate = function (position) {
+      this.left = position.left;
+      this.top = position.top;
+      this.right = this.left + this.w
+      this.bottom = this.top + this.h
     }
-    this.zoom = function(crop, zoom) {
-      this.x = (this.x - crop.w/2) * (zoom.previous/zoom.now) + crop.w/2;
-      this.y = (this.y - crop.h/2) * (zoom.previous/zoom.now) + crop.h/2;
+    this.update = function() {
+      crop_width = crop.w;
+      crop_height = crop.h;
+      crop_area = crop.w.toString() + 'x' + crop.w.toString();
+      scale = 1/zoom.now * 100;
     }
   }
   var Zoom = function(min, max) {
@@ -40,6 +49,18 @@
       onCropped: function() {}
     }, options);
 
+    if (settings.crop_width && settings.crop_height) {
+      var crop = {
+        w: settings.crop_width,
+        h: settings.crop_height
+      }
+    } else {
+      var crop = {
+        w: settings.crop_size,
+        h: settings.crop_size
+      }
+    }
+
     return this.each( function() {
       if (!$(this).is('img')) {
         return;
@@ -48,54 +69,39 @@
       var $image = $(this);
 
       $image.load( function(e) {
-        var image_size = new Box($image.width(), $image.height());
-
-        if (settings.crop_width && settings.crop_height) {
-          var crop = new Box(settings.crop_width, settings.crop_height);
-        } else {
-          var crop = new Box(settings.crop_size, settings.crop_size);
-        }
 
         // Start with image center at center of crop zone
-        var lefttop = new Offset((crop.w - image_size.original_w)/2, (crop.h - image_size.original_h)/2);
-        var zoom = new Zoom (Math.max(1, crop.w/image_size.original_w, crop.h/image_size.original_h), Math.min(image_size.original_w/crop.w, image_size.original_h/crop.h))
+        var image = new Image(
+          $image.width(), 
+          $image.height(),
+          (crop.w - $image.width())/2,
+          (crop.h - $image.height())/2
+        );
+
+        var zoom = new Zoom (Math.max(1, crop.w/image.o_w, crop.h/image.o_h), Math.min(image.o_w/crop.w, image.o_h/crop.h))
 
         $image.wrap('<div id="icon-crop"><div id="crop-box" style="width:' + crop.w + 'px; height: ' + crop.h + 'px" ></div></div>');
-        $image.css({'left': lefttop.x, 'top': lefttop.y, 'width': image_size.w, 'height': image_size.h });
+        $image.css({'left': image.left, 'top': image.top, 'width': image.w, 'height': image.h });
         $('<div id="icon-crop-slider"></div>').appendTo('#icon-crop')
         $('#icon-crop-slider').slider({
           min: zoom.min * 100,
           max: zoom.max * 100,
           slide: function( event, ui ) {
             zoom.now = ui.value/100;
-            image_size.zoom(zoom);
-            lefttop.zoom(crop, zoom);
-            $image.css({'left': lefttop.x, 'top': lefttop.y, 'width': image_size.w, 'height': image_size.h });
+            image.resize(zoom, crop);
+            $image.css({'left': image.left, 'top': image.top, 'width': image.w, 'height': image.h });
             zoom.previous = zoom.now;
-            updateCropData();
+            image.update;
+            settings.onCropped.call( this, image );
           }
         });
         $image.draggable({
           drag: function( event, ui) {
-            lefttop.x = ui.position.left;
-            lefttop.y = ui.position.top;
-            updateCropData();
+            image.translate(ui.position);
+            image.update;
+            settings.onCropped.call( this, image );
           }
         });
-        function updateCropData() {
-          if ( $.isFunction(settings.onCropped) ) {
-            crop_params = {
-              w: crop.w,
-              h: crop.h,
-              area: crop.w.toString() + 'x' + crop.w.toString(),
-              x: -lefttop.x,
-              y: -lefttop.y,
-              scale: 1/zoom.now * 100
-            }
-
-            settings.onCropped.call( this, crop_params );
-          }
-        }
       });
     });
   }
