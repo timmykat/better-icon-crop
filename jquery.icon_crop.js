c48f5563b382bfbc2af56c992141340d04b34036
Tim Kinnel <tim@vivoom.co>
2013-05-19 17:39:24 -0400

    Provide smaller data object

--- jquery.icon_crop.js
+++ jquery.icon_crop.js
@@ -8,28 +8,53 @@
     this.top = top = top || 0;
     this.right = this.left + w;
     this.bottom = this.top + h
-    this.original_w = w;
-    this.original_h = h;
-    this.resize = function(zoom, crop) {
-      this.w = this.original_w / zoom.now;
-      this.h = this.original_h / zoom.now;
+    this.crop = {
+      w: 100,
+      h: 100,
+      area: "100x100",
+      offset: {}
+    }
+    this.resize = function(zoom) {
+      this.w = this.o_w / zoom.now;
+      this.h = this.o_h / zoom.now;
       this.zoom = zoom.now
-      this.left = (this.left - crop.w/2) * (zoom.previous/zoom.now) + crop.w/2;
-      this.top = (this.top - crop.h/2) * (zoom.previous/zoom.now) + crop.h/2;
+      this.left = (this.left - this.crop.w/2) * (zoom.previous/zoom.now) + this.crop.w/2;
+      this.top = (this.top - this.crop.h/2) * (zoom.previous/zoom.now) + this.crop.h/2;
       this.right = this.left + this.w
       this.bottom = this.top + this.h
     }
     this.translate = function (position) {
       this.left = position.left;
       this.top = position.top;
-      this.right = this.left + this.w
-      this.bottom = this.top + this.h
+      this.right = this.left + this.w;
+      this.bottom = this.top + this.h;
     }
-    this.update = function() {
-      crop_width = crop.w;
-      crop_height = crop.h;
-      crop_area = crop.w.toString() + 'x' + crop.w.toString();
-      scale = 1/zoom.now * 100;
+    this.update = function(zoom) {
+      var scale = (1/zoom * 100).toFixed(2);
+      var area = this.crop.w.toString() + "x" + this.crop.h.toString();
+      var offset_x = -this.left;
+      var offset_y = -this.top;
+      var data = {
+        scale: scale,
+        crop: {
+          w: this.crop.w,
+          h: this.crop.h,
+          x: offset_x,
+          y: offset_y,
+          area: area
+        },
+        offset: {
+          x: - this.crop.w,
+          y: - this.crop.h
+        },
+        imagemagick: "-resize " + scale + "% -crop " + area + "+" + offset_x + "+" +  offset_y
+      }
+      return data;
+    }
+    this.set_crop = function (w, h) {
+      this.crop.w = w;
+      this.crop.h = h;
+      this.crop.area = this.crop.w.toString() + 'x' + this.crop.w.toString();
     }
   }
   var Zoom = function(min, max) {
@@ -49,18 +74,9 @@
       onCropped: function() {}
     }, options);
 
-    if (settings.crop_width && settings.crop_height) {
-      var crop = {
-        w: settings.crop_width,
-        h: settings.crop_height
-      }
-    } else {
-      var crop = {
-        w: settings.crop_size,
-        h: settings.crop_size
-      }
-    }
-
+    crop_w = settings.crop_width || settings.crop_size;
+    crop_h = settings.crop_height || settings.crop_size
+    
     return this.each( function() {
       if (!$(this).is('img')) {
         return;
@@ -74,13 +90,13 @@
         var image = new Image(
           $image.width(), 
           $image.height(),
-          (crop.w - $image.width())/2,
-          (crop.h - $image.height())/2
+          (crop_w - $image.width())/2,
+          (crop_h - $image.height())/2
         );
+        image.set_crop(crop_w, crop_h);
 
-        var zoom = new Zoom (Math.max(1, crop.w/image.o_w, crop.h/image.o_h), Math.min(image.o_w/crop.w, image.o_h/crop.h))
-
-        $image.wrap('<div id="icon-crop"><div id="crop-box" style="width:' + crop.w + 'px; height: ' + crop.h + 'px" ></div></div>');
+        var zoom = new Zoom (Math.max(1, image.crop.w/image.o_w, image.crop.h/image.o_h), Math.min(image.o_w/image.crop.w, image.o_h/image.crop.h))
+        $image.wrap('<div id="icon-crop"><div id="crop-box" style="width:' + image.crop.w + 'px; height: ' + image.crop.h + 'px" ></div></div>');
         $image.css({'left': image.left, 'top': image.top, 'width': image.w, 'height': image.h });
         $('<div id="icon-crop-slider"></div>').appendTo('#icon-crop')
         $('#icon-crop-slider').slider({
@@ -88,18 +104,18 @@
           max: zoom.max * 100,
           slide: function( event, ui ) {
             zoom.now = ui.value/100;
-            image.resize(zoom, crop);
+            image.resize(zoom);
             $image.css({'left': image.left, 'top': image.top, 'width': image.w, 'height': image.h });
             zoom.previous = zoom.now;
-            image.update;
-            settings.onCropped.call( this, image );
+            data = image.update(zoom.now);
+            settings.onCropped.call( this, data );
           }
         });
         $image.draggable({
           drag: function( event, ui) {
             image.translate(ui.position);
-            image.update;
-            settings.onCropped.call( this, image );
+            data = image.update(zoom.now);
+            settings.onCropped.call( this, data );
           }
         });
       });
